3d81f5488c6aeceab8dbcbd087f33b8582b705ae
Tusbeos <biconghi123@gmail.com>
2026-01-06 17:03:16 +0700

    add bulk and fetch doctor service & create model doctor service

---
+++ src/models/doctor_services.js
@@ -0,0 +1,24 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class DoctorServices extends Model {
+    static associate(models) {}
+  }
+  DoctorServices.init(
+    {
+      doctorId: DataTypes.INTEGER,
+      nameVi: DataTypes.STRING,
+      nameEn: DataTypes.STRING,
+      price: DataTypes.STRING,
+      description: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "DoctorServices",
+      tableName: "doctor_services",
+      freezeTableName: true,
+      timestamps: true,
+    }
+  );
+  return DoctorServices;
+};
